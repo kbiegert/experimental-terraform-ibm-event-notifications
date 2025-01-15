cf1e2b122a48f67b4db8a5f1893d023e87c2c3d9
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-15 10:57:24 -0500

    add do nothing terraform for ys1-staging testing only

---
+++ solutions/standard/main-staging.tf
@@ -0,0 +1,32 @@
+
+resource "time_sleep" "wait_9_seconds" {
+    create_duration = "9s"
+}
+
+output "event_notification_instance_name" {
+    value       = "event_notification_instance_name"
+}
+
+output "crn" {
+    value       = "crn"
+}
+
+output "guid" {
+    value       = "guid"
+}
+
+output "service_credentials_json" {
+    value       = "service_credentials_json"
+}
+
+output "service_credentials_object" {
+    value       = "service_credentials_object"
+}
+
+output "service_credential_secrets" {
+    value       = "service_credential_secrets"
+}
+
+output "service_credential_secret_groups" {
+    value       = "service_credential_secret_groups"
+}
